2c261dad0091e5dd1e41aa521368cc5e5e28af78
GTretiakov <g.tret1990@gmail.com>
2021-01-02 09:44:44 +0000

    js code added

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -1,7 +1,8 @@
 let myArray = [];
 
+let myArray2 = [];
+
 $("#playbtn").click(function(){
-    
     let a = Math.floor(Math.random() * 9 + 1);
     if (a == 1) {
         $("#one").addClass("highlight");
@@ -61,11 +62,75 @@ $("#playbtn").click(function(){
     console.log(myArray)
 });
 
-let m
+
 
 $("#one").click(function(){
+    myArray2.push(1);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
+$("#two").click(function(){
+    myArray2.push(2);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
+$("#three").click(function(){
+    myArray2.push(3);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
+$("#four").click(function(){
+    myArray2.push(4);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
+$("#five").click(function(){
+    myArray2.push(5);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
+$("#six").click(function(){
+    myArray2.push(6);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
+$("#seven").click(function(){
+    myArray2.push(7);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
+$("#eight").click(function(){
+    myArray2.push(8);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
+$("#nine").click(function(){
+    myArray2.push(9);
+    if (arrayEquals(myArray, myArray2) == true) {
+    console.log("success")
+}
+});
 
-})
+function arrayEquals(myArray, myArray2) {
+  return Array.isArray(myArray) &&
+    Array.isArray(myArray2) &&
+    myArray.length === myArray2.length &&
+    myArray.every((val, index) => val === myArray2[index]);
+};
+
+function play()
+
+
+
+
 
 
 
